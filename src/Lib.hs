7e0f12eeced53d052246ee86a4fe405583106081
luke <nerorevenge@gmail.com>
2017-06-16 01:12:26 -0400

    broken initial code

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,44 @@
 module Lib
-    ( someFunc
-    ) where
+  ( generateStart
+  , generateBlock
+  , getSampleDirectory
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad.IO.Class
+import qualified Data.ByteString as B
+import Data.List
+import Network.SSH.Client.SimpleSSH
+
+generateStart :: String
+generateStart = "[general]" ++ "\n" ++ "state_file = /var/awslogs/state/agent-state"
+
+generateBlock :: String -> String -> String -> String -> String
+generateBlock headerName streamName logGroupName fileName =
+  let header = "[" ++ headerName ++ "]"
+      logStreamName = "log_stream_name = " ++ streamName
+      file = " file = " ++ fileName
+      groupName = "log_group_name = " ++ logGroupName
+  in intercalate
+       "\n"
+       [ header
+       , logStreamName
+       , "datetime_format = %b %d %H:%M:%S"
+       , file
+       , groupName
+       , "initial_position = start_of_file"
+       , "buffer_duration = 5000"
+       ]
+
+getSampleDirectory :: IO ()
+getSampleDirectory =
+  (runSimpleSSH $
+   withSessionKey
+     ""
+     443
+     ""
+     ""
+     ""
+     ""
+     ""
+     (\x -> execCommand x "ls ")) >>=
+  either (putStrLn . show) (B.putStrLn . resultOut)
